d88f3b602720d7399ab2e9f08c73d563d1efbf70
Ankitdet <ankitdetroja@gmail.com>
2018-01-04 18:56:07 +0530

    display blank value instead of NULL

--- src/com/test/ws/utils/JacksonJaxbJsonProvider.java
+++ src/com/test/ws/utils/JacksonJaxbJsonProvider.java
@@ -5,10 +5,20 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.ext.Provider;
 import javax.ws.rs.core.MediaType;
 
+import com.fasterxml.jackson.databind.ser.DefaultSerializerProvider;
+import org.apache.cxf.tools.corba.idlpreprocessor.IncludeResolver;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.jaxrs.Annotations;
 import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+import org.codehaus.jackson.map.JsonSerializer;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.map.SerializerProvider;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.ser.StdSerializerProvider;
+
+import java.io.IOException;
 
 @Provider
 @Consumes({ MediaType.APPLICATION_JSON, "text/json" })
@@ -22,10 +32,20 @@ public class JacksonJaxbJsonProvider extends JacksonJsonProvider {
 		objectMapper = new ObjectMapper();
 		objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
 		objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
-		
+		/*objectMapper.setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);*/
+		StdSerializerProvider sp = new StdSerializerProvider();
+		sp.setNullValueSerializer(new NullSerializer());
+		objectMapper.setSerializerProvider(sp);
 	}
-	
+
 	public JacksonJaxbJsonProvider() {
 		super(objectMapper, DEFAULT_ANNOTATIONS);
 	}
+
+	public static class NullSerializer extends JsonSerializer<Object> {
+		public void serialize(Object value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+			jgen.writeString("");
+		}
+	}
 }
+
